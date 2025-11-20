a7146bc4493c35a3991856f9952ddc55a31197a0
VegiSaiKusumita29 <saikusumitav@gmail.com>
2025-11-20 20:45:26 +0530

    Add files via upload

---
+++ Fibinocii.py
@@ -0,0 +1,14 @@
+
+
+def fibonacci(n):
+    fib_series = []
+    a, b = 0, 1
+    for _ in range(n):
+        fib_series.append(a)
+        a, b = b, a + b
+    return fib_series
+
+
+n = 1000
+print("Fibonacci series up to", n, "terms:")
+print(fibonacci(n))
